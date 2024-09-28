c4717352e7356e61c097c1687820c6d55584fa62
Prashant Khadka <25191509+alis-khadka@users.noreply.github.com>
2024-09-28 13:27:38 -0500

    feat(): use activejob to complete the transactions

--- app/jobs/transaction_job.rb
+++ app/jobs/transaction_job.rb
@@ -1,7 +1,13 @@
 class TransactionJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  retry_on StandardError, attempts: 3
+
+  def perform(transaction_id)
+    transaction = Transaction.find(transaction_id)
+
+    transaction.completed!
+
+    transaction.failed! if !transaction.completed?
   end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,7 @@
 class Transaction < ApplicationRecord
     belongs_to :wallet
 
+    after_commit :process, on: [:create, :update]
     after_commit :update_wallet_balance, on: [:create, :update]
 
     enum status: {
@@ -13,15 +14,23 @@ class Transaction < ApplicationRecord
     private
     def process
         if self.timestamp.future?
-            
+            TransactionJob.set(wait_until: self.timestamp).perform_later(self.id)
         else
+            begin
+                self.completed!
 
+                self.failed! if !self.completed?
+            rescue ActiveRecord::RecordInvalid => error
+                Rails.logger.error "Failed to process transaction with id: #{self.id} and error: #{error.message}."
+            end
         end
     end
 
     def update_wallet_balance
-        Wallet.transaction do
-            self.wallet.update_total_balance
+        if saved_change_to_status? && completed?
+            Wallet.transaction do
+                self.wallet.update_total_balance
+            end
         end
     end
 end
